08995bae5eda32d495f2c2b028d8587b69a37b66
Inho Choi <hasbegun@gmail.com>
2020-11-06 21:35:15 -0800

    Add web chat services from MathX

--- scripts/web_chat_client.sh
+++ scripts/web_chat_client.sh
@@ -1,5 +1,5 @@
 PORT=34596
 HOST=0.0.0.0    # accept all IPs.
-python3 ../ParlAI/parlai/chat_service/services/browser_chat/client.py \
-    --host 0.0.0.0 \
+python3 ../ParlAI/parlai/chat_service/services/mathx_web/client.py \
+    --host $HOST \
     --port $PORT
--- scripts/web_chat_server.sh
+++ scripts/web_chat_server.sh
@@ -1,4 +1,4 @@
 PORT=34596
-sudo python3 ../ParlAI/parlai/chat_service/services/browser_chat/run.py \
-  --config-path ../ParlAI/parlai/chat_service/tasks/chatbot/config.yml \
+sudo python3 ../ParlAI/parlai/chat_service/services/mathx_web/run.py \
+  --config-path ../ParlAI/parlai/chat_service/tasks/mathx_chatbot/config.yml \
   --port $PORT
